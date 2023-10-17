f746262b1a09685e99f744d88d2021c2ec082e4b
GianmarcoDandrea <gianm96@hotmail.it>
2023-10-17 16:04:14 +0200

    add: js script

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// RACCOLTA DATI
+const user_name = prompt("Inserisci il tuo nome");
+console.log(user_name , typeof user_name );
+
+const user_surname = prompt("Inserisci il tuo cognome");
+console.log(user_surname , typeof user_surname);
+
+const fav_color = prompt("Inserisci il tuo colore preferito");
+console.log(fav_color, typeof fav_color);
+
+// LOGICA DEL PROGRAMMA
+const password = `${user_name}${user_surname}${fav_color}21`;
+console.log(password, typeof password);
+
+//OUTPUT
+
+document.getElementById("newpassword").innerHTML = password;
